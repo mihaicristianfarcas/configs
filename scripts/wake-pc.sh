c7a9c9ffd9d8c60965f5dfc29c871708cf69db0b
Mihai-Cristian Farcaș <146859576+mihaicristianfarcas@users.noreply.github.com>
2026-04-03 23:05:31 +0300

    feat: wake script more pretty

--- scripts/wake-pc.sh
+++ scripts/wake-pc.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-echo "---------------------------------------------------------------"
-echo "Sending magic packet via SSH..."
+printf ">> Sending magic packet via SSH...\n\n"
 ssh -v mihaicristianfarcas@raspberrypi 'wakeonlan A4:C3:F0:7C:0D:49'
 
-echo "---------------------------------------------------------------"
-echo "Sleep 15 seconds..."
-sleep 15
+printf "\n>> Sleep 15 seconds...\n\n"
 
-echo "---------------------------------------------------------------"
-echo "Try connection..."
+for i in $(seq 1 15);
+do
+    if [ $i -lt 15 ]; then
+        printf "$i, "
+    else
+	printf "$i!\n"
+    fi
+    sleep 1
+done
+
+printf "\n>> Try connection...\n\n"
 ping -c 5 windows
